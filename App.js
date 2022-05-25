198c37dba31077be622b14d9c0d8c330bcdb51ab
Dianna Ganova <diannaganova@yahoo.com>
2022-05-25 15:19:09 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+
+
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet, View, Button } from 'react-native'
+
+
+
+
+const App: () => Node = () => {
+
+
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+    <View style={{height: 100, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 16, textAlign: 'center'}}>Selfies</Text>
+    
+    </View>
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
